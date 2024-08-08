a9004b366644b9e219f01c59bb9076012b372b1a
vsharijijth <vsharijith0105@gmail.com>
2024-08-08 15:55:14 +0530

    deleting copies

--- 1. Band Name Generator - Copy.py
+++
@@ -1,4 +0,0 @@
-print("Welcome to Band Name Generator.")
-city_name = input("What's the name of the city you grew up in?\n")
-pets_name = input("What's your pet's name?\n")
-print("Your band name could be "+city_name+" "+pets_name)
